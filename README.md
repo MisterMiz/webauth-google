832a9f6f2ae81cd9e3f7600cdd9db0f612034db9
Surathat <6131302008@lamduan.mfu.ac.th>
2020-03-10 09:39:28 +0700

    initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Web authentication using passport.js and google
+## Using VScode for Git and IDE
+
+### branch master
+* npm init
+* npm i express passport ejs ...
+* create gitignore and add "node_modules"
---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const path = require("path");
+const app = express();
+
+app.set("view engine", "ejs");
+
+//Route(Services)
+app.get("/",(req,res) => {
+    // res.sendFile(path.join(__dirname, "views/home.html"));
+    res.render("home.ejs");
+});
+
+const PORT = 3000;
+
+app.listen(PORT, () => {
+    console.log("server is running at "+PORT);
+});
